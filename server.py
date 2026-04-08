32099565537afb5d599c7fe4aabbc0a4080bec3b
lightpillar-et <lightpillar2.0@gmail.com>
2026-04-08 14:30:27 +0200

    Add HTTP request parsing step 2.1 (split into lines and sections)

--- server.py
+++ server.py
@@ -15,7 +15,7 @@ print(f"Server is listening on {host}:{port}")
 
 # accept connectino
 connection_socket, client_address = server_socket.accept()
-print(f"Connection accepted from {client_address}")
+print(f"Connection accepted from {client_address} and Connection_Socket is  {connection_socket}")
 
 # receive raw data (bytes)
 raw_request_data = connection_socket.recv(1024)
@@ -25,6 +25,37 @@ print(raw_request_data)
 
 # decode bytes → string
 request = raw_request_data.decode()
+
+#  split the raw request into parts
+lines = request.split("\r\n")
+
+print("----- REQUEST LINES -----")
+print(lines)
+request_line = lines[0]
+print("----- REQUEST LINE -----")
+print(request_line)
+
+#Finding Where the Blank Line is in the HTTP request Body 
+blank_line_index =0
+for i in range(len(lines)):
+    if lines[i] == "":
+        blank_line_index = i
+        break
+
+print("----- BLANK LINE INDEX -----")
+print(blank_line_index)
+
+#Extracting the Header 
+header_lines = lines[1: blank_line_index]
+print("----- HEADER LINES -----")
+print(header_lines)
+
+#Extracting the Body
+body_lines = lines[blank_line_index + 1:]
+body = "\r\n".join(body_lines)
+
+print("----- BODY -----")
+print(body)
 
 # build HTTP response
 response = (
